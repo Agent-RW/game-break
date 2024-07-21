5e0256db7ea420f164f3815c2dddd0ccf5c46dd9
Ronald W <RonWingate11@gmail.com>
2024-07-21 14:25:34 -0400

    feat: Redesign game flow and logic to accomodate change in input type
    Refactor function playRound so complex steps are handled in function
    calls. Add structure to the base html file to avoid unnecessary
    delays in rendering. Make layout and style adjustments. Switch
    the player UI to a button based control system.

--- javascript.js
+++ javascript.js
@@ -1,95 +1,113 @@
-
-/* 
-Rock, Paper, Scissors Game in which the user plays against a bot
-
-GET user selection between rock, paper, and scissor
-COMPUTE computer rock, paper, and scissors selection using a random number
-IF the user's input is beats the computer's selection, the user wins
-ELIF the computer's input beats the user's input, the computer wins
-ELSE nobody wins
-*/
+const DISPLAY = document.querySelector("#display");
+const CONTROLS = document.querySelector("#controls");
+const TARGET_SCORE = 5;
 let playerScore;
 let computerScore;
 
 function getComputerChoice() {
-    let choice = (parseInt(Math.random() * 99) % 3);
+  let choice = parseInt(Math.random() * 99) % 3;
 
-    switch (choice){
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
-        default:
-            return "ERROR: Invalid Choice"
-    }
-
+  switch (choice) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+    default:
+      return "ERROR: Invalid Choice";
+  }
 }
 
-function getPlayerChoice() {
-    let playerChoice = prompt('Rock / Paper / Scissors');
-    return playerChoice.trim().toLocaleLowerCase();
+function playRound(playerChoice) {
+  let victor;
+  let computerChoice = getComputerChoice();
 
+  bestOption = compareOptions(playerChoice, computerChoice);
+
+  if (bestOption === "ERROR") {
+    return "Error handling needs to be added | Have player replay";
+  }
+
+  if (bestOption === "draw") {
+    victor = "Nobody";
+  } else if (bestOption === playerChoice) {
+    victor = "Player";
+    playerScore++;
+  } else {
+    victor = "Computer";
+    computerScore++;
+  }
+
+  SCORE_DISPLAY.textContent = `(Player) ${playerScore} : ${computerScore} (CPU)`;
+  ROUND_DISPLAY.textContent = `${playerChoice} vs. ${computerChoice} | ${victor}'s Point`;
+
+  if (playerScore >= TARGET_SCORE || computerScore >= TARGET_SCORE) {
+    //Called setTimeout otherwise alert will interrupt DOM rendering before score updates
+    setTimeout(() => {
+      alert(`GAME OVER | ${victor} Wins!!!`);
+    }, 1);
+    startNewGame();
+  }
 }
 
-function playRound(computerChoice, playerChoice) {
-    let choices = `Choice | Player ${playerChoice} : ${computerChoice} CPU | `;
-    let winAlert = 'Not too bad';
-    let loseAlert = 'You suck';
-    let drawAlert = 'Until next time';
-    let victor;
+function compareOptions(a, b) {
+  let bestOption;
 
-    if (playerChoice === computerChoice) {
-        alert(choices + drawAlert);
-    }else if (playerChoice === 'rock') {
-        if (computerChoice === 'scissors') {
-            alert(choices + winAlert)
-            victor = "player";
-        }else {
-            alert(choices + loseAlert);
-            victor = "cpu";
-        }
-    }else if (playerChoice === 'paper') {
-        if (computerChoice === 'rock') {
-            alert(choices + winAlert);
-            victor = "player";
-        }else {
-            alert(choices + loseAlert);
-            victor = "cpu";
-        }
-    }else if (playerChoice === 'scissors') {
-        if (computerChoice === 'paper') {
-            alert(choices + winAlert);
-            victor = "player";
-        }else {
-            alert(choices + loseAlert);
-            victor = "cpu";
-        }
-    }else {
-        alert('Have you never player rock, paper, scissors?');
+  if (a === b) {
+    bestOption = "draw";
+  } else if (a === "rock") {
+    if (b === "scissors") {
+      bestOption = a;
+    } else {
+      bestOption = b;
     }
+  } else if (a === "paper") {
+    if (b === "rock") {
+      bestOption = a;
+    } else {
+      bestOption = b;
+    }
+  } else if (a === "scissors") {
+    if (b === "paper") {
+      bestOption = a;
+    } else {
+      bestOption = b;
+    }
+  } else {
+    bestOption = "ERROR";
+  }
 
-    return victor;
+  return bestOption;
 }
 
 function onSelect(event) {
-    let selected = event.target.textContent;
-
-    playRound(getComputerChoice(), selected);
+  let playerChoice = event.target.textContent.toLocaleLowerCase();
+  playRound(playerChoice);
 }
 
-const rpsContainer = document.querySelector("div");
-const rockBtn = document.createElement("button");
-const paperBtn = document.createElement("button");
-const scissorsBtn = document.createElement("button");
-rockBtn.textContent = "Rock";
-paperBtn.textContent = "Paper";
-scissorsBtn.textContent = "Scissors";
+function startNewGame() {
+  playerScore = 0;
+  computerScore = 0;
 
+  SCORE_DISPLAY.textContent = `(Player) ${playerScore} : ${computerScore} (CPU)`;
+  ROUND_DISPLAY.textContent = "Choose Rock/Paper/Scissors to Begin"
+}
 
+const SCORE_DISPLAY = document.createElement("div");
+const ROUND_DISPLAY = document.createElement("div");
+DISPLAY.appendChild(SCORE_DISPLAY);
+DISPLAY.appendChild(ROUND_DISPLAY);
 
-[rockBtn, paperBtn, scissorsBtn].forEach((button) => {
-    button.addEventListener("click", onSelect);
-    rpsContainer.appendChild(button);
+const ROCK_BTN = document.createElement("button");
+const PAPER_BTN = document.createElement("button");
+const SCISSOR_BTN = document.createElement("button");
+ROCK_BTN.textContent = "Rock";
+PAPER_BTN.textContent = "Paper";
+SCISSOR_BTN.textContent = "Scissors";
+[ROCK_BTN, PAPER_BTN, SCISSOR_BTN].forEach((button) => {
+  button.addEventListener("click", onSelect);
+  CONTROLS.appendChild(button);
 });
+
+startNewGame();
